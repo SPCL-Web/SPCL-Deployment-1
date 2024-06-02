61718424389c567295a768c5a707ab36521e7194
MD NAZIM <131196179+Nazim452@users.noreply.github.com>
2024-06-02 19:41:11 +0530

    Second Time Deploying

--- index.js
+++ index.js
@@ -62,6 +62,54 @@
 
 // })
 
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import dbConnect from './config/db.js';
+// import authRoutes from './routes/authRoutes.js';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+// // Create __dirname for ES modules
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const app = express();
+
+// // Load config from .env file
+// dotenv.config();
+// const PORT = process.env.PORT || 4000;
+
+// // Middleware to parse JSON request body and enable CORS
+// app.use(express.json());
+// app.use(cors());
+
+// app.use(express.static(path.join(__dirname, './client/build')));
+
+// // Mount the API routes
+// app.use('/api/v1', authRoutes);
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server started successfully at ${PORT}`);
+// });
+
+// // Connect to the database
+// dbConnect();
+
+// // Serve the React application
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './client/build/index.html'));
+// });
+
+// // Default route
+// app.get('/', (req, res) => {
+//   res.send(`<h1>This is the Home Page</h1>`);
+// });
+
+
+
+
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -84,31 +132,23 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
+// Connect to the database
+dbConnect();
+
+// Serve the React application
 app.use(express.static(path.join(__dirname, './client/build')));
 
 // Mount the API routes
 app.use('/api/v1', authRoutes);
 
+// Catch-all route to serve React application for any route not handled by the above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server started successfully at ${PORT}`);
 });
-
-// Connect to the database
-dbConnect();
-
-// Serve the React application
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
-
-// Default route
-app.get('/', (req, res) => {
-  res.send(`<h1>This is the Home Page</h1>`);
-});
-
-
-
-
 
 
